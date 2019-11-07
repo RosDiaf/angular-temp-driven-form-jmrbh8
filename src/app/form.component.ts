19ae3c81e99d2b67db798370d770d8b535992b1f
RosDiaf <ros_diaf@hotmail.com>
2019-11-07 18:29:03 +0000

    2 files modified

--- src/app/form.component.ts
+++ src/app/form.component.ts
@@ -7,4 +7,15 @@ import { Component, Input } from '@angular/core';
 })
 export class FormComponent  {
   @Input() name: string;
+
+  onSubmit(heroForm) {
+    let details = {
+      city: heroForm.value.city,
+      firstname: heroForm.value.firstname,
+      lastname: heroForm.value.lastname,
+      street: heroForm.value.street,
+      zip: heroForm.value.zip,
+    }
+    console.log(details)
+  }
 }
